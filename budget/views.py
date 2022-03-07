b08551d5554731e534c8afc0b96ce10e70b15fb0
martabogdal <mnbogdal@gmail.com>
2022-03-08 00:11:48 +0100

    Add pagination and assign filters.

--- budget/views.py
+++ budget/views.py
@@ -3,8 +3,10 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .serializers import BudgetSerializer
 from .models import Budget
+from .filtersets import BudgetFilterSet
 
 
 class BudgetView(viewsets.ModelViewSet):
     serializer_class = BudgetSerializer
+    filterset_class = BudgetFilterSet
     queryset = Budget.objects.all()
--- family_budget/settings.py
+++ family_budget/settings.py
@@ -71,6 +71,8 @@ REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': (
         'django_filters.rest_framework.DjangoFilterBackend',
     ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 30
 }
 
 ROOT_URLCONF = 'family_budget.urls'
